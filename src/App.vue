9029a9fa3e2f1ff52f8f741c5f560c392aa66090
PhiRequiem <iphi@tuta.io>
2023-01-26 21:01:54 -0600

    empieza la magia

---
+++ src/App.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="container-fluid" data-bs-theme="light">
+    <div class="row">
+      <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="card text-bg-dark">
+          <div class="card-body big-card">
+            <h5 class="card-title">CheckList de seguridad en Wordpress</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis magni numquam laborum facilis, quaerat vitae minus dolorum distinctio veritatis recusandae sint? Dolorem doloremque culpa, sunt sequi labore nam possimus esse.</p>
+            <a href="#" class="btn btn-primary">Ver CheckList</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6 mb-3 mb-sm-0">
+        <div class="card text-bg-primary">
+          <div class="card-body big-card text-center">
+            <h5 class="card-title">Chequeo rapido</h5>
+            <div class="form-floating mb-3 text-dark text-center">
+              <input type="email" class="form-control" id="floatingInput" placeholder="tu URL">
+              <label for="floatingInput">tu URL</label>
+            </div>
+            <div class="d-grid gap-2">
+              <a href="#" class="btn btn-success">Chequea mi Wordpress</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top ">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Bitacora</a>
+            </li>
+
+            <li class="nav-item">
+              <a class="nav-link">Chequea</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">Recursos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link">Acerca de</a>
+            </li>
+          </ul>
+          <!-- <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form> -->
+        </div>
+      </div>
+    </nav>
+  
+  <div class="container">
+    <h1>Texto de introducción</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi totam alias quis consequuntur sint minus pariatur molestias nam, ipsam expedita dolore ut veritatis aut beatae cum tempora corrupti laudantium natus!
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam saepe facilis in ullam. Similique deleniti dignissimos sint eos reprehenderit magni dolor animi non eius. Minus officiis quae doloremque hic ipsum!
+    Enim beatae, nihil consequatur at fuga laborum! Dicta natus soluta consequuntur quos eum exercitationem doloremque repellat asperiores aliquid. Veniam numquam nihil nulla modi, expedita soluta cumque ullam cupiditate nemo esse?
+    Porro, consequuntur optio. Voluptatibus animi dolore debitis? Illo eligendi amet nesciunt modi excepturi veniam eaque praesentium nulla iure. Eos, quidem repudiandae. Dicta cumque nulla illo, ea recusandae sequi sit voluptatum.
+    Repellat laudantium nemo reprehenderit quod enim aut porro voluptate sunt minima quo, quidem id. Saepe blanditiis, deleniti quibusdam, fugit, ex tenetur necessitatibus alias nobis atque illo velit doloremque autem veniam!
+    Optio doloremque quisquam voluptatibus pariatur cum quasi nihil ab accusamus recusandae esse ipsam, tenetur neque voluptas asperiores saepe corporis eius similique soluta, repellendus iure maxime necessitatibus reprehenderit laborum repudiandae. Nemo.
+    Vel quibusdam accusantium vero dicta, natus velit numquam iure earum culpa sint eveniet aliquid impedit quaerat. Quidem, quod quis, corrupti maiores nihil voluptate totam ut hic dolores consectetur, aperiam amet?
+    Temporibus quam cumque doloremque, deleniti esse harum praesentium iste nemo animi ratione nulla at fugiat, voluptatibus dolorem quo ab repudiandae eum dolores earum labore sunt. Harum pariatur ut et accusantium?
+    Assumenda repellendus eaque aliquid a labore laboriosam quam facilis eum ducimus accusamus quis enim nostrum in veniam minima eius, distinctio quos doloremque eos aut nulla ipsa voluptatem? Tempora, cum hic?</p>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<!-- <script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style> -->
